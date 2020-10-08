055bbb08489158eb02757535eb299f9a2d2e073f
lusineduryan <lusineduryan@mail.ru>
2020-10-08 14:48:37 +0400

    complier finished

--- Inbox/compile.js
+++ Inbox/compile.js
@@ -5,5 +5,6 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'Contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).Contrats[':Inbox'];
+
 
