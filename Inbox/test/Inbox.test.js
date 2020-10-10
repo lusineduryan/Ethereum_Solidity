51a37ad5c2c24de1c72472a582e6c0d4112e0f7f
lusineduryan <lusineduryan@mail.ru>
2020-10-10 15:45:58 +0400

    test

---
+++ Inbox/test/Inbox.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } =  require('../compile');
+
+
+let accounts;
+let inbox;
+
+beforeEach(async () =>
+{
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // use one of those accounts to deploy the contract
+    //inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({ data: bytecode, arguments: ['hi'] }).send({ from: accounts[0], gas: '1000000'});
+
+});
+
+describe('Inbox', () =>
+{
+    it('deploys a contract', () =>
+    {
+        console.log(accounts);
+    });
+});
