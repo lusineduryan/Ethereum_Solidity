045d659a0881934a76cd0c5344b7c423e53fe756
lucy_duryan <lusineduryan@mail.ru>
2020-10-26 20:13:47 +0400

    assertion fix

--- Udemy/Inbox/test/Inbox.test.js
+++ Udemy/Inbox/test/Inbox.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+//const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
@@ -6,6 +6,7 @@ const { interface, bytecode } =  require('../compile');
 
 
 let accounts;
+
 let inbox;
 
 beforeEach(async () =>
