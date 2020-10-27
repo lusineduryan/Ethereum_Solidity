27c57c717429358f9e5066f3d7a4505abcd00e03
lucy_duryan <lusineduryan@mail.ru>
2020-10-27 19:47:23 +0400

    unit tests added

--- Udemy/Inbox/test/Inbox.test.js
+++ Udemy/Inbox/test/Inbox.test.js
@@ -1,8 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } =  require('../compile');
+
 
 
 let accounts;
@@ -23,5 +25,17 @@ describe('Inbox', () =>
     it('deploys a contract', () =>
     {
         console.log(accounts);
+        assert.ok(inbox.options.address);
     });
+
+    it("has a default message", async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, 'hi');
+    });
+
+    it("can change the message", async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(message, 'bye');
+    })
 });
