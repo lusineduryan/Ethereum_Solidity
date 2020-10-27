2370eb66c136e528290b4da1b52040c33b8ecba3
lucy_duryan <lusineduryan@mail.ru>
2020-10-27 20:06:54 +0400

    deploy added

--- Udemy/Inbox/deploy.js
+++ Udemy/Inbox/deploy.js
@@ -2,5 +2,24 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
+const provider = new HDWalletProvider(
+    'average sound random talk fiscal canyon response track owner title tornado ticket',
+    'https://rinkeby.infura.io/v3/e92299e9adb146dfb9cb27885fa92025'
+);
 
+const web3 = new Web3(provider);
 
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const res = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', res.options.address);
+
+};
+
+deploy();
